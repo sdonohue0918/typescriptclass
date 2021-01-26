8efff90e9c4612ee5f861fb3c6a856361c77a15c
Sean Donohue <sdonohue0918@gmail.com>
2021-01-26 16:50:01 -0500

    classes code moved to own file, index cleared

--- index.js
+++ index.js
@@ -1,134 +1,104 @@
-// function startup(){
-//     console.log("page loaded")
-// }
-// startup()
-// function add(n1: number, n2: number) {
-//     return n1 + n2
-// }
-// const num1 = 5
-// const num2 = 10
-// const toggle = false
-// //const result = add(num1, num2)
-// function add1(n1: number, n2: number, t: boolean) {
-//     if (t){
-//         return n1 + n2
-//     } else {
-//         console.log(n1 + n2)
-//     }
-// }
-// add1(num1, num2, toggle)
-var Sean = {
-    name: "Sean",
-    age: 26,
-    role: [2, 'programmer']
-};
-var seanTyped = {
-    name: "Sean",
-    age: 26,
-    hobbies: ['reading', 'gaming'],
-    role: [1, 'programmer']
-};
-var product = {
-    id: 'abc1',
-    price: 12.99,
-    tags: ['great offer', 'hot-and-new'],
-    details: {
-        title: 'Red Carpet',
-        description: 'A great carpet - almost brand-new!'
+class Department {
+    constructor(n, id) {
+        this.employees = [];
+        this.name = n;
+        this.code = id;
     }
-};
-var Role;
-(function (Role) {
-    Role[Role["a"] = 0] = "a";
-    Role[Role["b"] = 1] = "b";
-    Role[Role["c"] = 2] = "c";
-})(Role || (Role = {}));
-//will assign a number to these variables by default but you can also assign values as well
-//console.log(Role.a)
-console.log(seanTyped.role);
-//console.log(product.tags)
-//when you see data typed with an array next to it like test: string[] it means 'array of strings'
-// test: any[] means you can have any data type in the array
-// test1: [string, number] TUPLE --> use to specify what data types, where in the array, and the amount of elements
-//Union type
-function add(input1, input2) {
-    var result;
-    if (typeof input1 === 'number' && typeof input2 === 'number') {
-        result = input1 + input2;
+    //constructor method creates an instance of the class
+    // constructor(private readonly n: string, public id: string ) {
+    // }
+    //can also be initialized like the above with access modifiers 
+    //readonly can only be added in constructor and prevents that value from being modified by any class methods
+    describe() {
+        console.log('Department' + this.name);
     }
-    else {
-        result = input1.toString() + input2.toString();
+    //using the this parameter in ts is interpreted in ts as describing that this will be referring to an instance of the 'Department' Class which adds a layer of data security to the code
+    addEmployees(employee) {
+        this.employees.push(employee);
     }
-    return result;
-}
-var returned = add(30, 'Anna');
-//console.log(returned)
-//Literal type
-function combine1(input1, input2, resultConver) {
-    var result;
-    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConver === 'asNumber') {
-        result = +input1 + +input2;
-    }
-    else {
-        result = input1.toString() + input2.toString();
-    }
-    if (resultConver === 'asNumber') {
-        return +result;
-    }
-    else {
-        return result.toString();
+    printInfo() {
+        console.log(this.employees.length);
+        console.log(this.employees);
     }
 }
-//you use type (from ts) to give a name for custom type 
-function combine2(input1, input2, resultConver) {
-    var result;
-    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConver === 'asNumber') {
-        result = +input1 + +input2;
+const researchdevelopment = new Department('R&D', 'JhTTyajk');
+//console.log(researchdevelopment)
+//researchdevelopment.addEmployees('Sean')
+//researchdevelopment.printInfo()
+class ITDept extends Department {
+    constructor(admins, reports) {
+        super('IT', 'XjjiTcJ');
+        this.admins = admins;
+        this.reports = reports;
+        this.admins = admins;
+        this.reports = reports;
     }
-    else {
-        result = input1.toString() + input2.toString();
+    //super must be added to the constructor of any class that extends a parent class, super calls the constructor of the base class 
+    //super must be called before using 'this' to declare specific properties
+    addEmployees(name) {
+        if (name !== 'doosh') {
+            this.employees.push(name);
+        }
+        else {
+            return;
+        }
     }
-    if (resultConver === 'asNumber') {
-        return +result;
+    //this class method overrides the base class method with its own functionality, allowable and we can access employees data by using a protected access modifier flag on employees list in the base class
+    addReport(report) {
+        this.reports.push(report);
     }
-    else {
-        return result.toString();
+    get lastReport() {
+        if (this.reports.length === 0) {
+            return;
+        }
+        else {
+            return this.reports[this.reports.length - 1];
+        }
+    }
+    set removeReport(reportNo) {
+        if (this.reports[reportNo]) {
+            this.reports.splice(reportNo, 1);
+        }
+        else {
+            return;
+        }
+    }
+    static createNotice(memo) {
+        return memo;
     }
 }
-//Return Type
-//assigned to the function to check for correct return value data type on compilation
-function add2(inp1, inp2) {
-    return inp1 + inp2;
+//Singleton Pattern --> ensuring that you can only have one instance of a class
+//"strictPropertyInitialization": false
+class AccountingDept extends Department {
+    constructor() {
+        super('Accounting', 'HjjtyR');
+    }
+    static getInstance() {
+        if (AccountingDept.instance) {
+            return this.instance;
+        }
+        else {
+            this.instance = new AccountingDept();
+            return this.instance;
+        }
+    }
 }
-// let test = add2(2,5)
-// console.log(test)
-//Return Type/Void Type
-//a function with a void return type doesn't return any value, in other programming languages, undefined is what happens in vanilla js 
-//also will compile if you do return something, making void flexible in cases where you may or may not want to return a value depending on the control flow
-function printResult(num) {
-    console.log('Result: ' + num);
-}
-//printResult(add(3,4))
-//Functions as Types
-//() => creates a function type
-var combineValues;
-//this syntax/semant basically infers that combineValues can accept an assigned function  with params that are nums and that will return a number value, a function that is assigned that doesn't meet these specs will cause a compilation error 
-combineValues = add2;
-//console.log(combineValues(3, 6))
-//Unknown Type
-//you can store any value in unknown without getting errors on compilation (like any-type) 
-var userInput;
-//function types and callbacks
-function addAndHandle(num1, num2, callback) {
-    var result = num1 + num2;
-    callback(result);
-}
-// addAndHandle(5,10, (result) => {
-//     console.log(result)
-// })
-//never function type
-function generateError(message, code) {
-    throw { message: message,
-        errorCode: code };
-}
-generateError('an error occurred', 401);
+const IT = new ITDept(['doosh', 'bag', 'leroy'], []);
+IT.addEmployees('Dingus');
+IT.addEmployees('bach');
+IT.addReport('shitters clogged');
+IT.addReport('HR blows');
+IT.addReport('Hr sucks');
+//IT.removeReport = 2
+//setters take the parameter like the above; cant use method(param) syntax
+//console.log(IT.lastReport)
+//getters are called by classInst.methodName without ()
+const message = ITDept.createNotice('we need to dev');
+//console.log(message)
+//console.log(IT)
+const accountingDept = AccountingDept.getInstance();
+const accountingAudits = AccountingDept.getInstance();
+//console.log(accountingDept, accountingAudits)
+//ON ABSTRACT CLASSES
+// use abstract classes when you want derived classes to inherit methods that will have unique implementations 
+//Abstract classes need to be extended by derived class, you cannot make instances of them
